7eabd3102119fdcc10ba76628d0cf6251bdd10ba
alex <alex.olucas@hotmail.com>
2021-01-04 21:44:41 -0300

    Project Created

---
+++ backend/src/test/java/com/dev/devdeliver/DevdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.devdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
