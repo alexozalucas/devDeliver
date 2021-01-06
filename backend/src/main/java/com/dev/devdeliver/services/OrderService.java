fdf502b8a165b82c3f123aa054d578820e180d6c
alex <alex.olucas@hotmail.com>
2021-01-06 18:01:24 -0300

    OrderDto Created

---
+++ backend/src/main/java/com/dev/devdeliver/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.dev.devdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dev.devdeliver.dto.OrderDTO;
+import com.dev.devdeliver.dto.ProductDTO;
+import com.dev.devdeliver.entities.Order;
+import com.dev.devdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findAll();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+
+	}
+
+}
